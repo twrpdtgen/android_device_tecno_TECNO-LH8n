601220c78365a5c9b49ff9fa1bc000e4b8abe32d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-07-12 17:48:54 +0200

    TECNO-LH8n: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: tecno Model: TECNO LH8n Script version: 3.0.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for TECNO TECNO LH8n (TECNO-LH8n)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ omni_TECNO-LH8n.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from TECNO-LH8n device
+$(call inherit-product, device/tecno/TECNO-LH8n/device.mk)
+
+PRODUCT_DEVICE := TECNO-LH8n
+PRODUCT_NAME := omni_TECNO-LH8n
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO LH8n
+PRODUCT_MANUFACTURER := tecno
+
+PRODUCT_GMS_CLIENTID_BASE := android-tecno
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_lh8nu_h333-user 12 SP1A.210812.016 639565 release-keys"
+
+BUILD_FINGERPRINT := TECNO/LH8n-GL/TECNO-LH8n:12/SP1A.210812.016/240617V454:user/release-keys

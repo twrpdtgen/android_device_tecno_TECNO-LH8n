9627e4e05cffe87850dfe6befb25aef92312199b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-09-04 13:48:42 +0200

    TECNO-LH8n: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: tecno Model: TECNO LH8n Script version: 3.0.0

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_TECNO-LH8n-user
+add_lunch_combo omni_TECNO-LH8n-userdebug
+add_lunch_combo omni_TECNO-LH8n-eng
